6322616b669b7ea1c82fe7eb6e9fa96cec635b18
LLF <569141891@qq.com>
2022-09-16 23:10:56 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/tree">Tree</router-link>|
+      <router-link to="/axios">Axios</router-link>
+
     </nav>
     <router-view/>
   </div>
