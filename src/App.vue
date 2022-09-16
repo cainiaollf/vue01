4af117a30c0a00e7d77b570c8505cdda3751fcc9
LLF <569141891@qq.com>
2022-09-16 23:14:17 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
       <router-link to="/tree">Tree</router-link>|
-      <router-link to="/axios">Axios</router-link>
+      <router-link to="/axios">Axios</router-link> |||
 
     </nav>
     <router-view/>
